b6f49aa7cf792f98eea62bc784dd917ab3c51848
Joao <joao.campos@unemat.br>
2021-02-19 14:55:01 -0400

    inicia a integrar menu mobile ao painel do wordpress

--- header.php
+++ header.php
@@ -1,102 +1,22 @@
-<!DOCTYPE html>
-<html <?php language_attributes();?>>
-
-<head>
-  <meta <?php bloginfo('charset'); ?> />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/header.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animacoes.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/footer.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/cards.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/global-styles.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/fonts.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/section.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/map.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/midia-luiz.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/midia-joao.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/toggle.css"/>
-  
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
-  <?php wp_head();?>
-</head>
-
-<body>
-<div class="baner-inicio">
     <header>
       <div class="menu-icones">
         <div class="logo-Aginov">
           <img src="<?php bloginfo('template_url'); ?>/img/logo-aginov-desk.svg" alt="" />
         </div>
-        
         <?php      
-       $options =  array(
-    'items_wrap'        => '%3$s', 
-    
-    'menu_class'        => false, 
-    'menu_id'           => false,
-    'container'         => 'div',
-    'container_class'   => 'navbar-principal',
-    'container_id'      => false,
-    'walker' => new WP_Bootstrap_Navwalker(),
-);
-
-$menu = wp_nav_menu($options);
-
-echo strip_tags($menu, '');
+          $options =  array(
+                'items_wrap'        => '%3$s', 
+                
+                'menu_class'        => false, 
+                'menu_id'           => false,
+                'container'         => 'div',
+                'container_class'   => 'navbar-principal',
+                'container_id'      => false,
+                'walker' => new WP_Bootstrap_Navwalker(),
+            );
+          $menu = wp_nav_menu($options);
+          echo strip_tags($menu, '');
         ?>
-        <!-- <nav class="navbar-principal">
-          <div class="navbar-item">
-            <p class="inicio-op"> Início</p>
-            <div class="barra-navbar"></div>
-            <ul class="inicio d-none">
-              <li>A agencia</li>
-              <li>Empresa</li>
-              <li>Teste</li>
-              <li>menu1</li>
-            </ul>
-          </div>
-          <div class="navbar-item">
-            <p class="menu2-op">Portfólio</p>
-            <div class="barra-navbar"></div>
-            <ul class="menu2 d-none">
-              <li>menu2</li>
-              <li>menu2</li>
-              <li>menu2</li>
-              <li>menu2</li>
-            </ul>
-          </div>
-          <div class="navbar-item">
-            <p class="menu3-op">Parcerias</p>
-            <div class="barra-navbar"></div>
-            <ul class="menu3 d-none">
-              <li>menu3</li>
-              <li>menu3</li>
-              <li>menu3</li>
-              <li>menu3</li>
-            </ul>
-          </div>
-          <div class="navbar-item">
-            <p class="empreend-op">Empreendedorismo</p>
-            <div class="barra-navbar"></div>
-            <ul class="empreend d-none">
-              <li>Cursos e Eventos</li>
-              <li>Empresas Júnior</li>
-              <li>Incubadoras da Unemat</li>
-              <li>Ecossistema de inovação</li>
-              <li>Disciplinas de Empreendedorismo</li>
-            </ul>
-          </div>
-          <div class="navbar-item">
-            <p class="contato-op">Contato</p>
-            <div class="barra-navbar"></div>
-            <ul class="contato d-none">
-              <li>menu4</li>
-              <li>menu4</li>
-              <li>menu4</li>
-              <li>menu4</li>
-            </ul>
-          </div>
-        </nav> -->
         <div class="area-pesq">
           <div class="logo-vitrine">
             <img id="brasao" src="<?php bloginfo('template_url'); ?>/img/brasao-unemat.svg" alt=""
@@ -130,6 +50,20 @@ echo strip_tags($menu, '');
           <img class="btn-drop" src="<?php bloginfo('template_url'); ?>/img/menu-dropdown-close.svg" onclick=" menu()"
             alt="">
           <div class="menu-list">
+            <?php      
+            $options =  array(
+                  'items_wrap'        => '%3$s', 
+                  
+                  'menu_class'        => false, 
+                  'menu_id'           => false,
+                  'container'         => 'div',
+                  'container_class'   => 'navbar-principal',
+                  'container_id'      => false,
+                  'walker' => new WP_Bootstrap_Navwalker(),
+              );
+            $menu = wp_nav_menu($options);
+            echo strip_tags($menu, '');
+          ?>
             <ul class="opcao-list">
               <li class="elements-list"><a class="item-menu-color" id="menu-drop-1" href="#">
                   <p class="iten-list-option">Inicio</p>
@@ -206,41 +140,3 @@ echo strip_tags($menu, '');
       <img class="bg-header-wave zindex mr-03" src="<?php bloginfo('template_url'); ?>/img/background-header.svg" />
 
     </header>
-    <section>
-      <img class="bg-header-cicle-1" src="<?php bloginfo('template_url'); ?>/img/circles-line.svg" />
-      <img class="bg-header-square-1" src="<?php bloginfo('template_url'); ?>/img/square.svg" alt="" />
-      <div class="cards-header">
-        <div class="card-h-item">
-          <p class="card-h-titulo">
-            <strong> Transferência </strong> de Tecnologia
-          </p>
-          <a class="btn-saibamais">Saiba Mais</a>
-          <div class="card-h-body">
-            <img src="<?php bloginfo('template_url'); ?>/img/img-computer-vitrine.svg" alt="" />
-          </div>
-        </div>
-        <div class="card-h-item">
-          <p class="card-h-titulo">
-            <strong> Alianças </strong> Estratégicas
-          </p>
-          <a class="btn-saibamais">Saiba Mais</a>
-          <div class="card-h-body">
-            <img src="<?php bloginfo('template_url'); ?>/img/img-computer-vitrine.svg" alt="" />
-          </div>
-        </div>
-        <div class="card-h-item">
-          <p class="card-h-titulo">
-            <strong> Empreendedorismo </strong> e Logística
-          </p>
-          <a class="btn-saibamais">Saiba Mais</a>
-          <div class="card-h-body">
-            <img src="<?php bloginfo('template_url'); ?>/img/img-computer-vitrine.svg" alt="" />
-          </div>
-        </div>
-      </div>
-    </section>
-    <img class="bg-header-wave-2 img-bg temp2" src="<?php bloginfo('template_url'); ?>/img/background-midias2.svg" alt="">
-    <img class="bg-header-wave-2 img-bg" src="<?php bloginfo('template_url'); ?>/img/background-midias2.svg" alt="">
-    <img class="bg-header-wave-2 img-bg temp3" src="<?php bloginfo('template_url'); ?>/img/background-midias.svg" alt="">
-  <img class="bg-header-wave-2 img-bg temp1" src="<?php bloginfo('template_url'); ?>/img/background-midias3.svg" alt="">
-  </div>
