1aaba821a058cc4235ee502803c33412a58ac32a
Joao <joao.campos@unemat.br>
2021-03-03 14:03:44 -0400

    Move o conteudo do head para o header.php, adiciona o arquivo menu.php onde ficaram os menus dinamicos, remve chamada do header dos arquivos e adiciona nas paginas principais

--- banner-portifolio.php
+++ banner-portifolio.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html <?php language_attributes();?>>
 
-<head>
-  <meta <?php bloginfo('charset'); ?> />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/header.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animacoes.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/footer.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/cards.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/global-styles.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/fonts.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/section.css" />
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/map.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/midia-luiz.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/midia-joao.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/toggle.css" />
-
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
-  <?php wp_head();?>
-</head>
-
-<body>
   <div class="baner-inicio">
-    <?php get_header(); ?>
+    <?php include('menu.php'); ?>
     <input id="templateUrl" class="d-none" type="text" value="<?php bloginfo('template_url')?>">
     <section>
       <img class="bg-header-cicle-1" src="<?php bloginfo('template_url'); ?>/img/circles-line.svg" />
