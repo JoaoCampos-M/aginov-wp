0cfef520af2189739df93bf079b2eff8bcfc7b1c
Joao <joao.campos@unemat.br>
2021-03-29 21:17:10 -0400

    adiciona waves e rodape de contatos nas paginas single propriedade e propriedade catetgoria 2

--- propriedade-page.php
+++ propriedade-page.php
@@ -2,12 +2,14 @@
 <?php get_header(); ?>
 
 <?php include('menu.php'); ?>
-<section class="section-info container d-flex">
+<div>
+  <section class="section-info container d-flex">
 
-	<div class="menu-info">
-		<img class="desktop img-bn-portifolio" src="<?php bloginfo('template_url'); ?>/img/img-computer-propriedade.svg" alt="">
+    <div class="menu-info">
+      <img class="desktop img-bn-portifolio" src="<?php bloginfo('template_url'); ?>/img/img-computer-propriedade.svg"
+        alt="">
 
-		<?php
+      <?php
 		$my_args = array(
 			'post_type'=>'propriedade',
 			'category_name' => 'PropriedadePage',
@@ -16,30 +18,30 @@
 
 		$my_query = new WP_Query($my_args);
 		?>
-		<?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
-				<!--
+      <?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
+      <!--
     <div class="item-menu-info i-selected">
       <strong class="text-legenda m-dados">Item Selecionado</strong>
       <p class="text-legenda m-dados">Descrição do item</p>
     </div>
     -->
-		<a href="<?php the_permalink(); ?>" class="item-menu-info">
-			<strong class="text-legenda m-dados"><?php the_title(); ?></strong>
-			<!-- <p class="text-legenda m-dados">Descrição do item</p> -->
-		</a>
-		<?php endwhile;
+      <a href="<?php the_permalink(); ?>" class="item-menu-info">
+        <strong class="text-legenda m-dados"><?php the_title(); ?></strong>
+        <!-- <p class="text-legenda m-dados">Descrição do item</p> -->
+      </a>
+      <?php endwhile;
 		endif;
 		wp_reset_query() ?>
-		<div class="buttons-list">
-			<a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedade/">page1</a>
-			<a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedadepage/">page2</a>
-		</div>
+      <div class="buttons-list">
+        <a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedade/">page1</a>
+        <a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedadepage/">page2</a>
+      </div>
 
-	</div>
+    </div>
 
-	<div class="info-content">
-		<h1 class="info-principal-title">Propriedade Intelectual</h1>
-		<?php
+    <div class="info-content">
+      <h1 class="info-principal-title">Propriedade Intelectual</h1>
+      <?php
 			$my_args = array(
 				'post_type'=>'propriedade',
 				'category_name' => 'PropriedadePage',
@@ -48,18 +50,24 @@
 			$my_query = new WP_Query($my_args);
 		?>
 
-		<?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
-		<div class="description-item">
-			<a href="<?php the_permalink(); ?>">
-				<h4 class="paragraph-title"><?php the_title(); ?></h4>
-			</a>
-			<div class="d-flex border-l m-tb-10">
-				<div class="info-txt"><?php the_content(); ?> </div>
-			</div>
-		</div>
-		<?php endwhile;
+      <?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?>
+      <div class="description-item">
+        <a href="<?php the_permalink(); ?>">
+          <h4 class="paragraph-title"><?php the_title(); ?></h4>
+        </a>
+        <div class="d-flex border-l m-tb-10">
+          <div class="info-txt"><?php the_content(); ?> </div>
+        </div>
+      </div>
+      <?php endwhile;
 		endif; ?>
-		<?php wp_reset_query(); ?>
-</section>
+      <?php wp_reset_query(); ?>
+  </section>
+  <img class="bg-contatos-prop img-bg" src="<?php bloginfo('template_url'); ?>/img/background-midias2.svg" alt="">
+  <img class="bg-contatos-prop img-bg temp1" src="<?php bloginfo('template_url'); ?>/img/background-midias2.svg" alt="">
+  <img class="bg-contatos-prop img-bg" src="<?php bloginfo('template_url'); ?>/img/background-midias.svg" alt="">
+  <img class="bg-contatos-prop img-bg temp5" src="<?php bloginfo('template_url'); ?>/img/background-midias3.svg" alt="">
+</div>
+<?php include("template_parts/transparencia-publica.php");?>
 
 <?php get_footer() ?>
--- single-propriedade.php
+++ single-propriedade.php
@@ -9,50 +9,57 @@ include('menu.php');
 ?>
 
 <?php $t="oi"; $c=""; $cat="";?>
-
-<section class="section-info container d-flex">
-<?php if(have_posts()) : the_post(); ?>
+<div>
+	<section class="section-info container d-flex">
+	<?php if(have_posts()) : the_post(); ?>
 	<?php
-		$t = get_the_title();
-		$c = get_the_content();
-		$categories = get_the_category($pos->ID);
-		foreach ( $categories as $category ) {
-			$cat = $category->cat_name;
-		}
+			$t = get_the_title();
+			$c = get_the_content();
+			$categories = get_the_category($pos->ID);
+			foreach ( $categories as $category ) {
+				$cat = $category->cat_name;
+			}
 
-	?>
-<?php endif;?>
-<div class="menu-info">
-  <img class="desktop img-bn-portifolio" src="<?php bloginfo('template_url'); ?>/img/img-computer-propriedade.svg" alt="">
+		?>
+	<?php endif;?>
+	<div class="menu-info">
+		<img class="desktop img-bn-portifolio" src="<?php bloginfo('template_url'); ?>/img/img-computer-propriedade.svg"
+			alt="">
 
-<?php
-   $my_args = array(
-		'post_type'=>'propriedade',
-		'category_name' => $cat,
-		'posts_per_page' => 3
-  	);
-	$my_query =new WP_Query($my_args);
-?>
-  <?php if( $my_query->have_posts()) : while( $my_query->have_posts() ) : $my_query->the_post(); ?>
-    <a href="<?php the_permalink();?>" class="item-menu-info">
-      <strong class="text-legenda m-dados"><?php the_title();?></strong>
-      <!-- <p class="text-legenda m-dados">Descrição do item</p> -->
-    </a>
-    <?php endwhile; endif;  wp_reset_query();?>
-    <div class="buttons-list">
-	 	<a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedade/">page1</a>
-		<a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedade-intelectual/propriedadepage/">page2</a>
-    </div>
-  </div>
-  <div class="info-content">
-  <h1 class="info-principal-title">Propriedade Intelectual</h1>
-  <div class="description-item">
+		<?php
+		$my_args = array(
+			'post_type'=>'propriedade',
+			'category_name' => $cat,
+			'posts_per_page' => 3
+		);
+		$my_query =new WP_Query($my_args);
+		?>
+		<?php if( $my_query->have_posts()) : while( $my_query->have_posts() ) : $my_query->the_post(); ?>
+		<a href="<?php the_permalink();?>" class="item-menu-info">
+			<strong class="text-legenda m-dados"><?php the_title();?></strong>
+			<!-- <p class="text-legenda m-dados">Descrição do item</p> -->
+		</a>
+		<?php endwhile; endif;  wp_reset_query();?>
+		<div class="buttons-list">
+			<a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedade/">page1</a>
+			<a class="btn-menu" href="http://aginov.risc.lan/index.php/propriedade-intelectual/propriedadepage/">page2</a>
+		</div>
+	</div>
+	<div class="info-content">
+		<h1 class="info-principal-title">Propriedade Intelectual</h1>
+		<div class="description-item">
 			<a>
-				<h4 class="paragraph-title"><?php echo $t; ?></h4>
+			<h4 class="paragraph-title"><?php echo $t; ?></h4>
 			</a>
 			<div class="d-flex border-l m-tb-10">
-				<div class="info-txt"><?php echo $c;?></div>
+			<div class="info-txt"><?php echo $c;?></div>
 			</div>
+		</div>
+	</section>
+	<img class="bg-contatos-prop img-bg" src="<?php bloginfo('template_url'); ?>/img/background-midias2.svg" alt="">
+	<img class="bg-contatos-prop img-bg temp1" src="<?php bloginfo('template_url'); ?>/img/background-midias2.svg" alt="">
+	<img class="bg-contatos-prop img-bg" src="<?php bloginfo('template_url'); ?>/img/background-midias.svg" alt="">
+	<img class="bg-contatos-prop img-bg temp5" src="<?php bloginfo('template_url'); ?>/img/background-midias3.svg" alt="">
 	</div>
-</section>
+	<?php include("template_parts/transparencia-publica.php");?>
 <?php get_footer(); ?>
